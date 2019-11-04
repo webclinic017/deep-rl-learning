8a5b21d1011e3762a47df927ae981eb02153946a
Thinh Le <thinh.le@boot.ai>
2019-11-04 09:35:48 +0100

    change policy actions

--- DDPG/ddpg.py
+++ DDPG/ddpg.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 import logging
-
+from random import random, randrange
 logging.basicConfig(filename='log/ddpg.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 from tqdm import tqdm
 from .actor import Actor
@@ -22,6 +22,9 @@ class DDPG:
         self.act_range = act_range
         self.env_dim = (k,) + env_dim
         self.gamma = gamma
+        self.epsilon = 1
+        self.epsilon_decay = 0.99
+        self.min_epsilon = 0.2
         self.lr = lr
         # Create actor and critic networks
         self.actor = Actor(self.env_dim, act_dim, act_range, 0.1 * lr, tau)
@@ -31,7 +34,12 @@ class DDPG:
     def policy_action(self, s):
         """ Use the actor to predict value
         """
-        return self.actor.predict(s)[0]
+        # return self.actor.predict(s)[0]
+        # return np.random.choice(np.arange(self.act_dim), 1, p=self.actor.predict(s).ravel())[0]
+        if random() <= self.epsilon:
+            return randrange(self.act_dim)
+        else:
+            return np.argmax(self.actor.predict(s)[0])
 
     def bellman(self, rewards, q_values, dones):
         """ Use the Bellman Equation to compute the critic target
@@ -65,6 +73,8 @@ class DDPG:
         # Transfer weights to target networks at rate Tau
         self.actor.transfer_weights()
         self.critic.transfer_weights()
+        if self.epsilon > self.min_epsilon:
+            self.epsilon = self.epsilon * self.epsilon_decay
 
     def train(self, env, args, summary_writer):
         results = []
@@ -84,16 +94,16 @@ class DDPG:
                 # Actor picks an action (following the deterministic policy)
                 a = self.policy_action(old_state)
                 # Clip continuous values to be valid w.r.t. environment
-                a = np.clip(a+noise.generate(time), -self.act_range, self.act_range)
+                # a = np.clip(a+noise.generate(time), -self.act_range, self.act_range)
                 # Retrieve new state, reward, and whether the state is terminal
-                new_state, r, done, info = env.step(np.argmax(a))
+                new_state, r, done, info = env.step(a)
                 # Display score
                 logging.warning(a)
                 logging.warning(info)
                 tqdm_e.set_description("Profit: " + str(round(info['total_profit'], 7)))
                 tqdm_e.refresh()
                 # Add outputs to memory buffer
-                self.memorize(old_state, a, r, done, new_state)
+                self.memorize(old_state, self.actor.predict(old_state)[0], r, done, new_state)
                 # Sample experience from buffer
                 states, actions, rewards, dones, new_states, _ = self.sample_batch(args.batch_size)
                 # Predict target q-values using target networks
