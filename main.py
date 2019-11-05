55b860413358b6f1ac9c130b614212e722fbdf17
Thinh Le <thinh.le@boot.ai>
2019-11-05 14:11:14 +0100

    set done

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def main(args=None):
         state_dim = (2,)
         action_space = (2,)
         action_dim = 2
-        act_range = 10
+        act_range = 1
     else:
         # Standard Environments
         # env = Environment(gym.make(args.env), args.consecutive_frames)
