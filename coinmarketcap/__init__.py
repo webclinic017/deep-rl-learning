f1817848f7462f87cbfe4169bdb701a887b181aa
Thinh Le <thinh.le@boot.ai>
2020-03-13 17:11:03 +0700

    save

--- coinmarketcap/__init__.py
+++ coinmarketcap/__init__.py
@@ -39,11 +39,11 @@ def convert_data(obj):
 
 
 # fetch test data
-klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, "1 Feb, 2015")
+klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "1 Feb, 2015")
 df_test = pd.DataFrame(klines, columns=['open_time', 'Open', 'High', 'Low', 'Close',
                                    'Volume', 'close_time', 'quote_asset_volume', 'number_of_trades',
                                    'buy_base_asset_volume', 'buy_quote_asset_volume', 'ignore'])
-df_test.to_csv('../data/1hour.csv', sep=',')
+df_test.to_csv('../data/15minute.csv', sep=',')
 # test_data = list(map(convert_data, klines))
 # print(test_data[0])
 
