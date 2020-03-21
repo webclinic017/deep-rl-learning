43830f61d9fb6f9895cb105dbbc9d056bba146e0
thinh <thinhlx1993@gmail.com>
2020-03-21 13:45:23 +0700

    ROC 95%

--- RT/realtime.py
+++ RT/realtime.py
@@ -1,95 +1,141 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import time
+import logging
 import pandas as pd
+import matplotlib.pyplot as plt
+from binance.enums import *
+from binance.client import Client
+from binance.enums import KLINE_INTERVAL_1HOUR
+from binance.websockets import BinanceSocketManager
+from pymongo import MongoClient
+
+logging.basicConfig(filename='../log/realtime.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
-class TrendAnalytics:
+class RealTimeAnalytics:
     def __init__(self):
-        self.data = pd.read_csv("../data/train.csv")
-        self.real_price = self.data.c.values
-        self.moving_avg = self.data.c.rolling(window=14).mean().fillna(7248).values
-        self.timestamp = self.data.timestamp.values
-        self.moving = list()
-        self.moving_data = list()
-        self.fig = plt.figure()
-        self.ax1 = self.fig.add_subplot(211, label='ax1')
-        self.ax2 = self.fig.add_subplot(212, label='ax2')
+        self.api_key = "y9JKPpQ3B2zwIRD9GwlcoCXwvA3mwBLiNTriw6sCot13IuRvYKigigXYWCzCRiul"
+        self.api_secret = "uUdxQdnVR48w5ypYxfsi7xK6e6W2v3GL8YrAZp5YeY1GicGbh3N5NI71Pss0crfJ"
+        self.binace_client = Client(self.api_key, self.api_secret)
+        self.bm = BinanceSocketManager(self.binace_client)
+        self.client = MongoClient()
+        self.db = self.client.crypto
+
+        # env
+        self.windows = 500
+        self.trading_data = []
+        self.delta = []
+        self.prev_timestamp = 0
+        self.prev_price = 0
         self.budget = 0
         self.order = 0
-        self.windows = 100
-        self.exp5 = []
-        self.exp6 = []
-        self.exp4 = []
-        self.exp7 = []
-        self.start_idx = 150
-        self.end_idx = -1
+        self.total_profit = 0
+        self.total_loss = 0
 
-    def plot_data(self):
-        prev_price = 7248
-        prev_timestamp = 1577775996.11432
-        for price, moving in zip(self.moving_avg, self.timestamp):
-            diff = (price - prev_price) / (moving - prev_timestamp)
-            diff = diff if abs(diff) > 0.03 else 0
-            prev_timestamp = moving
-            prev_price = price
-            self.moving.append(diff)
-
-        moving_df = pd.DataFrame(self.moving, columns=['ma'])
-        moving_df = moving_df.ma.rolling(window=14).mean().fillna(0).values
-        self.moving_data = moving_df
-        self.ax1.plot(moving_df[self.start_idx:self.end_idx], label='Moving')
-        self.ax2.plot(self.real_price[self.start_idx:self.end_idx], label='Raw data')
-
-    def test_trading(self):
-        _idx = self.start_idx
-        total_profit = 0
-        total_loss = 0
-        for current_moving, price in zip(self.moving_data[self.start_idx:self.end_idx], self.real_price[self.start_idx:self.end_idx]):
-            std_moving = self.moving_data[_idx - 5]
-            first_moving = self.moving_data[_idx - 7]
-
-            if self.moving_avg[_idx-75] < self.moving_avg[_idx]:
-                if not self.order and first_moving > std_moving < current_moving < -0.06:
-                    # buy signal
-                    self.exp4.append(_idx - self.start_idx)
-                    self.exp5.append(price)
-                    self.order = price
-
-                elif not self.order and abs(current_moving - std_moving) > 0.01 and 0.01 > current_moving > 0 and first_moving < std_moving < current_moving:
-                    self.exp4.append(_idx - self.start_idx)
-                    self.exp5.append(price)
-                    self.order = price
-
-            elif self.order and first_moving < std_moving > current_moving >= -0.02 and self.moving_avg[_idx-70] > self.moving_avg[_idx]:
-                self.exp6.append(_idx - self.start_idx)
-                self.exp7.append(price)
-                diff = price - self.order
-                self.budget += diff
-                if diff > 0:
-                    total_profit += diff
-                else:
-                    total_loss += diff
-
-                self.order = 0
-
-            elif self.order and min(self.real_price[_idx-14:_idx]) < self.order:
-                self.exp6.append(_idx - self.start_idx)
-                self.exp7.append(price)
-                diff = price - self.order
-                total_loss += diff
-                self.order = 0
-                self.budget += diff
-
-            _idx += 1
-
-        # self.ax2.plot(self.exp4, self.exp5, 'ro', color='g')
-        # self.ax2.plot(self.exp6, self.exp7, 'ro', color='r')
+        # matplotlib
+        # self.fig = plt.figure()
+        # self.ax1 = self.fig.add_subplot(211, label='ax1')
+        # self.ax2 = self.fig.add_subplot(212, label='ax2')
         # plt.show()
-        print("profit: {} total loss: {} total profit : {}".format(self.budget, total_loss, total_profit))
+        # self.exp5 = []
+        # self.exp6 = []
+        # self.exp4 = []
+        # self.exp7 = []
+
+    def start_socket(self):
+        # start any sockets here, i.e a trade socket
+        conn_key = self.bm.start_kline_socket('BTCUSDT', self.process_message, interval=KLINE_INTERVAL_1MINUTE)
+        # then start the socket manager
+        self.bm.start()
+
+    def process_message(self, msg):
+        msg = msg['k']
+        current_time = time.time()
+        msg['timestamp'] = current_time
+        _open = float(msg['o'])
+        _high = float(msg['h'])
+        _low = float(msg['l'])
+        _close = float(msg['c'])
+        _timestamp = msg['timestamp']
+        _latest = msg['x']
+
+        self.trading_data.append(_close)
+
+        # calculate diff
+        diff = (_close - self.prev_price) / (current_time - self.prev_timestamp)
+        diff = diff if abs(diff) > 0.03 else 0
+        self.prev_timestamp = current_time
+        self.prev_price = _close
+
+        self.delta.append(diff)
+
+        moving_df = pd.DataFrame(self.delta, columns=['ma'])
+        moving_df = moving_df.ma.rolling(window=14).mean().fillna(0).values
+
+        log_txt = "Price: {} | Budget: {} | Total Loss: {} | Total Profit: {}".format(_close, round(self.budget, 2),
+                                                                                      round(self.total_loss, 2),
+                                                                                      round(self.total_profit, 2))
+        logging.warning(log_txt)
+
+        if len(self.trading_data) >= self.windows:
+            self.trading_data.pop(0)
+            self.delta.pop(0)
+            self.trader(moving_df, self.trading_data)
+        insert = self.db.btc1minutes.insert_one(msg).inserted_id
+
+    def trader(self, moving_df, prices):
+        raw_df = pd.DataFrame(prices, columns=['c'])
+        moving_avg = raw_df.c.rolling(window=14).mean().fillna(prices[0]).values
+        # self.ax1.plot(moving_df, label='Moving')
+        # self.ax2.plot(prices, label='Raw data')
+        price = prices[-1]
+        current_moving = moving_df[-1]
+        std_moving = moving_df[-5]
+        first_moving = moving_df[-7]
+
+        # if self.moving_avg[_idx-75] < self.moving_avg[_idx]:
+        if not self.order and first_moving > std_moving < current_moving < -0.05:
+            # buy signal
+            log_txt = "Buy Order: {}".format(price)
+            logging.warning(log_txt)
+            # self.exp4.append(len(moving_df))
+            # self.exp5.append(price)
+            self.order = price
+
+        elif not self.order and abs(current_moving - std_moving) > 0.01 and 0.01 > current_moving > 0 and first_moving < std_moving < current_moving:
+            # self.exp4.append(len(moving_df))
+            # self.exp5.append(price)
+            log_txt = "Buy Order: {}".format(price)
+            logging.warning(log_txt)
+            self.order = price
+
+        elif self.order and first_moving < std_moving > current_moving >= -0.02 and moving_avg[-70] < moving_avg[-1]:
+            # self.exp6.append(len(moving_df))
+            # self.exp7.append(price)
+            diff = price - self.order
+            self.budget += diff
+            if diff > 0:
+                log_txt = "Take Profit: {} | From {} to {}".format(diff, self.order, price)
+                logging.warning(log_txt)
+                self.total_profit += diff
+            else:
+                log_txt = "Loss: {} | From {} to {}".format(diff, self.order, price)
+                logging.warning(log_txt)
+                self.total_loss += diff
+
+            self.order = 0
+
+        elif self.order and min(self.trading_data[-50:]) > self.order:
+            # self.exp6.append(len(self.trading_data))
+            # self.exp7.append(price)
+
+            diff = price - self.order
+            log_txt = "Stop Loss: {} | From {} to {}".format(diff, self.order, price)
+            logging.warning(log_txt)
+            self.total_loss += diff
+            self.order = 0
+            self.budget += diff
 
 
 if __name__ == '__main__':
-    bot = TrendAnalytics()
-    bot.plot_data()
-    bot.test_trading()
-
+    bot = RealTimeAnalytics()
+    bot.start_socket()
---
+++ autoencoder/main.py
@@ -0,0 +1,136 @@
+import pandas as pd
+from keras import Sequential
+from keras.layers import Dense
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+from keras.wrappers.scikit_learn import KerasRegressor, KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
+from matplotlib import pyplot
+
+class Autoencoder:
+    def __init__(self):
+        self.train_data, self.normalize_price, self.raw_price, self.moving_avg = self.get_data('../data/1hour.csv')
+        self.cci = self.calculate_cci(self.train_data, 25)
+        self.train = self.macd(self.cci)
+        self.train = self.train.dropna()
+        self.budget = 0
+
+    @staticmethod
+    def normalize(raw_list):
+        """Scale feature to 0-1"""
+        min_x = min(raw_list)
+        max_x = max(raw_list)
+        if not min_x or not max_x:
+            return raw_list
+
+        return_data = list(map(lambda x: (x - min_x) / (max_x - min_x), raw_list))
+        return return_data
+
+    def get_data(self, csv_path):
+        df = pd.read_csv(csv_path, sep=',')
+        normalize_price = self.normalize(df.Close.values)
+        raw_price = df.Close.values
+        moving_avg = df.Close.rolling(window=9).mean().dropna().values
+        return df, normalize_price, raw_price, moving_avg
+
+    @staticmethod
+    def macd(data_frame):
+        data = data_frame.copy()
+        ma = data_frame.Close.rolling(window=14).mean().fillna(0)
+        exp1 = data.Close.ewm(span=12, adjust=False).mean()
+        exp2 = data.Close.ewm(span=26, adjust=False).mean()
+        macd = exp1 - exp2
+
+        signal = macd.ewm(span=9, adjust=False).mean()
+        histogram = macd - signal
+        return_data = data_frame.join(pd.Series(signal, name='Signal'))
+        return_data = return_data.join(pd.Series(histogram, name='Histogram'))
+        return_data = return_data.join(pd.Series(macd, name='MACD'))
+        return_data = return_data.join(pd.Series(ma, name='MA'))
+        return return_data
+
+    @staticmethod
+    def calculate_cci(dataraw, ndays):
+        """Commodity Channel Index"""
+        tp = (dataraw['High'] + dataraw['Low'] + dataraw['Close']) / 3
+        raw_cci = pd.Series((tp - tp.rolling(ndays).mean()) / (0.015 * tp.rolling(ndays).std()), name='CCI')
+        return_data = dataraw.join(raw_cci)
+        return return_data
+
+    # define base model
+    def baseline_model(self):
+        # create model
+        model = Sequential()
+        model.add(Dense(512, input_dim=1, activation='relu'))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+        # Compile model
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=["accuracy"])
+        return model
+
+    def train_start(self):
+        train_data = self.train[['CCI']]
+        self.train['signal'] = 0
+        self.train.loc[(self.train.Close > self.train.MA), 'signal'] = 1
+        # self.train.loc[(self.train.Close < self.train.Open), 'signal'] = 0
+
+        y = self.train.signal
+        X_train, X_test, y_train, y_test = train_test_split(train_data, y, test_size=0.2)
+        print("Done")
+        # lm = linear_model.LinearRegression(normalize=True)
+        # model = lm.fit(X_train, y_train)
+        # predictions = lm.predict(X_test)
+        # for correct, prediction in zip(y, predictions):
+        #     print(correct, prediction)
+
+        # evaluate model
+        standardscaler = StandardScaler()
+        standardscaler.fit_transform(X_train)
+        model = KerasClassifier(build_fn=self.baseline_model, epochs=1000, batch_size=5, verbose=1)
+        # kfold = KFold(n_splits=2)
+        # results = cross_val_score(model, X_train, y_train, cv=kfold)
+        # print("Standardized: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+        model.fit(X_train, y_train)
+        standardscaler.transform(X_test)
+        y_pred = model.predict(X_test)
+        y_true = y_test
+        print(model.predict_proba(X_test))
+        print(accuracy_score(y_true, y_pred))
+        lr_probs = model.predict_proba(X_test)
+        lr_probs = lr_probs[:, 1]
+        # calculate scores
+        ns_probs = [0 for _ in range(len(y_test))]
+        ns_auc = roc_auc_score(y_test, ns_probs)
+        lr_auc = roc_auc_score(y_test, lr_probs)
+        # summarize scores
+        print('No Skill: ROC AUC=%.3f' % (ns_auc))
+        print('Logistic: ROC AUC=%.3f' % (lr_auc))
+        # calculate roc curves
+        ns_fpr, ns_tpr, _ = roc_curve(y_test, ns_probs)
+        lr_fpr, lr_tpr, _ = roc_curve(y_test, lr_probs)
+        # plot the roc curve for the model
+        pyplot.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+        pyplot.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+        # axis labels
+        pyplot.xlabel('False Positive Rate')
+        pyplot.ylabel('True Positive Rate')
+        # show the legend
+        pyplot.legend()
+        # show the plot
+        pyplot.show()
+
+        # print(y_pred)
+
+        # estimator = KerasRegressor(build_fn=self.baseline_model, epochs=100, batch_size=5, verbose=1)
+        # kfold = KFold(n_splits=10)
+        # results = cross_val_score(estimator, X_train, y_train, cv=kfold)
+        # print("Baseline: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
+
+if __name__ == '__main__':
+    autoencode = Autoencoder()
+    autoencode.train_start()
