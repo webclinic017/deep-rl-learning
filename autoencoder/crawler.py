6adc234415ad98bf1233668dde107f38fafa2928
ThinhPC <thinhlx1993@gmail.com>
2020-04-18 12:02:08 +0700

    new crawler file

--- autoencoder/crawler.py
+++ autoencoder/crawler.py
@@ -1,67 +1,44 @@
 import time
 import logging
-from binance.enums import KLINE_INTERVAL_1MINUTE, KLINE_INTERVAL_1HOUR, KLINE_INTERVAL_5MINUTE, SIDE_SELL, \
-    ORDER_TYPE_MARKET, SIDE_BUY, KLINE_INTERVAL_15MINUTE
+from binance.enums import KLINE_INTERVAL_1HOUR
 from pymongo import MongoClient
+from bson import ObjectId
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 
-logging.basicConfig(filename='../log/autotrade.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='log/crawler.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
 class RegressionMA:
     def __init__(self):
-        self.client = MongoClient(username='crypto', password='amyAFpNK', authSource='crypto')
+        self.client = MongoClient(host='66.42.37.163', username='admin',
+                                  password='jkahsduk12387a89sdjk@#', authSource='admin')
         self.db = self.client.crypto
+        self.api_key = "9Hj6HLNNMGgkqj6ngouMZD1kjIbUb6RZmIpW5HLiZjtDT5gwhXAzc20szOKyQ3HW"
+        self.api_secret = "ioD0XICp0cFE99VVql5nuxiCJEb6GK8mh08NYnSYdIUfkiotd1SZqLTQsjFvrXwk"
 
-    def start_socket_5m(self):
-        api_key = "9Hj6HLNNMGgkqj6ngouMZD1kjIbUb6RZmIpW5HLiZjtDT5gwhXAzc20szOKyQ3HW"
-        api_secret = "ioD0XICp0cFE99VVql5nuxiCJEb6GK8mh08NYnSYdIUfkiotd1SZqLTQsjFvrXwk"
-        binace_client = Client(api_key, api_secret)
+    def start_socket(self, market):
+        binace_client = Client(self.api_key, self.api_secret)
         # start any sockets here, i.e a trade socket
         bm = BinanceSocketManager(binace_client)
-        conn_key = bm.start_kline_socket('BTCUSDT', self.process_message_5m, interval=KLINE_INTERVAL_5MINUTE)
+        conn_key = bm.start_kline_socket(market, self.process_message, interval=KLINE_INTERVAL_1HOUR)
         # then start the socket manager
         bm.start()
 
-    def process_message_5m(self, msg):
-        msg['timestamp'] = time.time()
-        insert = self.db.btc_5m_test.insert_one(msg).inserted_id
-        print("Data 5m: {}".format(msg['k']['c']))
-
-    def start_socket_1h(self):
-        api_key = "9Hj6HLNNMGgkqj6ngouMZD1kjIbUb6RZmIpW5HLiZjtDT5gwhXAzc20szOKyQ3HW"
-        api_secret = "ioD0XICp0cFE99VVql5nuxiCJEb6GK8mh08NYnSYdIUfkiotd1SZqLTQsjFvrXwk"
-        binace_client = Client(api_key, api_secret)
-        # start any sockets here, i.e a trade socket
-        bm = BinanceSocketManager(binace_client)
-        conn_key = bm.start_kline_socket('BTCUSDT', self.process_message_1h, interval=KLINE_INTERVAL_1HOUR)
-        # then start the socket manager
-        bm.start()
-
-    def process_message_1h(self, msg):
+    def process_message(self, msg):
         msg['k']['timestamp'] = time.time()
-        insert = self.db.btc_1h_test.insert_one(msg).inserted_id
-        print("Data 1h: {}".format(msg['k']['c']))
-
-    def start_socket_15m(self):
-        api_key = "9Hj6HLNNMGgkqj6ngouMZD1kjIbUb6RZmIpW5HLiZjtDT5gwhXAzc20szOKyQ3HW"
-        api_secret = "ioD0XICp0cFE99VVql5nuxiCJEb6GK8mh08NYnSYdIUfkiotd1SZqLTQsjFvrXwk"
-        binace_client = Client(api_key, api_secret)
-        # start any sockets here, i.e a trade socket
-        bm = BinanceSocketManager(binace_client)
-        conn_key = bm.start_kline_socket('BTCUSDT', self.process_message_15m, interval=KLINE_INTERVAL_15MINUTE)
-        # then start the socket manager
-        bm.start()
-
-    def process_message_15m(self, msg):
-        msg['timestamp'] = time.time()
-        insert = self.db.btc_15m_test.insert_one(msg).inserted_id
-        print("Data 15M: {}".format(msg['k']['c']))
+        msg['_id'] = str(ObjectId())
+        market = msg['s']
+        collection = '{}_{}'.format(market, KLINE_INTERVAL_1HOUR)
+        insert = self.db[collection].insert(msg)
+        txt = "{} Data {}: {}".format(market, KLINE_INTERVAL_1HOUR, msg['k']['c'])
+        print(txt)
+        logging.warning(txt)
 
 
 if __name__ == '__main__':
     bottrading = RegressionMA()
-    bottrading.start_socket_5m()
-    bottrading.start_socket_1h()
-    bottrading.start_socket_15m()
+    symbols_list = ['BTCUSDT', 'ZEC_BTC', 'DASH_BTC', 'BNB_BTC', 'LINK_BTC', 'XTZ_BTC', 'ONT_BTC', 'XRP_BTC', 'ADA_BTC',
+                    'XLM_BTC', 'EOS_BTC', 'NEO_BTC']
+    for symbol in symbols_list:
+        bottrading.start_socket(symbol)
