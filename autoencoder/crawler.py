3fbca50975a9be95dda7aad37bba453a24231abc
ThinhPC <thinhlx1993@gmail.com>
2020-04-18 15:51:47 +0700

    new crawler

--- autoencoder/crawler.py
+++ autoencoder/crawler.py
@@ -1,3 +1,4 @@
+import threading
 import time
 import logging
 from binance.enums import KLINE_INTERVAL_1HOUR
@@ -10,17 +11,18 @@ logging.basicConfig(filename='log/crawler.log', filemode='w', format='%(name)s -
 
 
 class RegressionMA:
-    def __init__(self):
+    def __init__(self, market):
         self.client = MongoClient(username='admin', password='jkahsduk12387a89sdjk@#', authSource='admin')
         self.db = self.client.crypto
         self.api_key = "9Hj6HLNNMGgkqj6ngouMZD1kjIbUb6RZmIpW5HLiZjtDT5gwhXAzc20szOKyQ3HW"
         self.api_secret = "ioD0XICp0cFE99VVql5nuxiCJEb6GK8mh08NYnSYdIUfkiotd1SZqLTQsjFvrXwk"
+        self.market = market
 
-    def start_socket(self, market):
+    def start_socket(self):
         binace_client = Client(self.api_key, self.api_secret)
         # start any sockets here, i.e a trade socket
         bm = BinanceSocketManager(binace_client)
-        conn_key = bm.start_kline_socket(market, self.process_message, interval=KLINE_INTERVAL_1HOUR)
+        conn_key = bm.start_kline_socket(self.market, self.process_message, interval=KLINE_INTERVAL_1HOUR)
         # then start the socket manager
         bm.start()
 
@@ -36,7 +38,8 @@ class RegressionMA:
 
 
 if __name__ == '__main__':
-    symbols_list = ['BTCUSDT']
+    symbols_list = ['BTCUSDT', 'ZECBTC', 'DASHBTC', 'BNBBTC', 'LINKBTC', 'XTZBTC', 'ONTBTC', 'XRPBTC', 'ADABTC',
+                    'XLMBTC', 'EOSBTC', 'NEOBTC']
     for symbol in symbols_list:
-        bottrading = RegressionMA()
-        bottrading.start_socket(symbol)
+        bottrading = RegressionMA(symbol)
+        bottrading.start_socket()
