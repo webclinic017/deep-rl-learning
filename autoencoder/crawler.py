71307de6e39e712b3cd43a5f325f630ef76ec574
ThinhPC <thinhlx1993@gmail.com>
2020-04-29 21:44:42 +0700

    edit crawler

--- autoencoder/crawler.py
+++ autoencoder/crawler.py
@@ -1,7 +1,7 @@
 import threading
 import time
 import logging
-from binance.enums import KLINE_INTERVAL_1HOUR
+from binance.enums import KLINE_INTERVAL_1HOUR, KLINE_INTERVAL_5MINUTE
 from pymongo import MongoClient
 from bson import ObjectId
 from binance.client import Client
@@ -11,18 +11,19 @@ logging.basicConfig(filename='log/crawler.log', filemode='w', format='%(name)s -
 
 
 class RegressionMA:
-    def __init__(self, market):
+    def __init__(self, market, interval):
         self.client = MongoClient(username='admin', password='jkahsduk12387a89sdjk@#', authSource='admin')
         self.db = self.client.crypto
         self.api_key = "9Hj6HLNNMGgkqj6ngouMZD1kjIbUb6RZmIpW5HLiZjtDT5gwhXAzc20szOKyQ3HW"
         self.api_secret = "ioD0XICp0cFE99VVql5nuxiCJEb6GK8mh08NYnSYdIUfkiotd1SZqLTQsjFvrXwk"
         self.market = market
+        self.interval = interval
 
     def start_socket(self):
         binace_client = Client(self.api_key, self.api_secret)
         # start any sockets here, i.e a trade socket
         bm = BinanceSocketManager(binace_client)
-        conn_key = bm.start_kline_socket(self.market, self.process_message, interval=KLINE_INTERVAL_1HOUR)
+        conn_key = bm.start_kline_socket(self.market, self.process_message, interval=self.interval)
         # then start the socket manager
         bm.start()
 
@@ -30,16 +31,18 @@ class RegressionMA:
         msg['k']['timestamp'] = time.time()
         msg['_id'] = str(ObjectId())
         market = msg['s']
-        collection = '{}_{}'.format(market, KLINE_INTERVAL_1HOUR)
+        collection = '{}_{}'.format(market, self.interval)
         insert = self.db[collection].insert(msg)
-        txt = "{} Data {}: {}".format(market, KLINE_INTERVAL_1HOUR, msg['k']['c'])
+        txt = "{} Data {}: {}".format(market, self.interval, msg['k']['c'])
         print(txt)
         logging.warning(txt)
 
 
 if __name__ == '__main__':
-    symbols_list = ['BTCUSDT', 'ZECBTC', 'DASHBTC', 'BNBBTC', 'LINKBTC', 'XTZBTC', 'ONTBTC', 'XRPBTC', 'ADABTC',
-                    'XLMBTC', 'EOSBTC', 'NEOBTC']
-    for symbol in symbols_list:
-        bottrading = RegressionMA(symbol)
-        bottrading.start_socket()
+    symbols_list = ['BTCUSDT']
+    symbol = 'BTCUSDT'
+    crawler_1 = RegressionMA(symbol, KLINE_INTERVAL_1HOUR)
+    crawler_1.start_socket()
+
+    crawler_2 = RegressionMA(symbol, KLINE_INTERVAL_5MINUTE)
+    crawler_2.start_socket()
