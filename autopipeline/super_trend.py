87d0b39df2022f427f75a50f64ca46c95c7b1959
Thinh Le <thinh.le@boot.ai>
2021-12-09 11:27:04 +0700

    SAVE

--- autopipeline/super_trend.py
+++ autopipeline/super_trend.py
@@ -85,7 +85,7 @@ def scheduler_job():
 if __name__ == '__main__':
     # Run job every hour at the 42rd minute
     # scheduler_job()
-    schedule.every().hours.at(":01").do(scheduler_job)
+    schedule.every().days.do(scheduler_job)
     while True:
         schedule.run_pending()
         time.sleep(1)
